7000167944a1931da7c02f635ff72232909258fc
mochamadmaulana <mochamad.maulana@raharja.info>
2020-02-05 02:17:57 +0700

    stateLess component & stateFull component

---
+++ src/component/statefull/StateFullComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StateFullComponent.css';
+
+class StateFullComponent extends React.Component {
+   render(){
+      return <p className="text-p">StateFull Component</p>;
+   }
+}
+
+export default StateFullComponent;
---
+++ src/component/stateless/StateLessComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './StateLessComponent.css';
+
+const StateLessComponent = () => {
+   return <p className="text-p">Stateless Components atau Functional Components</p>;
+}
+
+export default StateLessComponent;
